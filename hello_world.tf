549c649c57ca4d08fb49070e8180dc17e2f23fb8
Aung Zan Baw <aungzanbaw@gmail.com>
2023-02-09 20:04:14 +0900

    Test with aws

--- hello_world.tf
+++ hello_world.tf
@@ -1,9 +1,41 @@
-resource "local_file" "hello_world_localfile" {
-  content = "Hello World from Terraform"
-  filename = "${path.module}/hello_world_local.txt"
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] 
 }
 
-resource "local_file" "hello_world_globalfile" {
-  content = "Hello Global from Terraform"
-  filename = "${path.module}/hello_world_global.txt"
+variable "region" {
+  type=string
 }
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "web" {
+  ami = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Hello World"
+    Platform = "Terraform"
+  }
+}
+# resource "local_file" "hello_world_localfile" {
+#   content = "Hello World from Terraform"
+#   filename = "${path.module}/hello_world_local.txt"
+# }
+
+# resource "local_file" "hello_world_globalfile" {
+#   content = "Hello Global from Terraform"
+#   filename = "${path.module}/hello_world_global.txt"
+# }
