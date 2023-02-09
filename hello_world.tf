0fa5c2d941538bb7bde16488cc14dd8f7b3dde8b
Aung Zan Baw <aungzanbaw@gmail.com>
2023-02-09 18:22:36 +0900

    Add global file

--- hello_world.tf
+++ hello_world.tf
@@ -2,3 +2,8 @@ resource "local_file" "hello_world_localfile" {
   content = "Hello World from Terraform"
   filename = "${path.module}/hello_world_local.txt"
 }
+
+resource "local_file" "hello_world_globalfile" {
+  content = "Hello Global from Terraform"
+  filename = "${path.module}/hello_world_global.txt"
+}
